a3391581314ffaa68780857b0eab0cbb2d2644ae
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-05-13 11:26:17 +0200

    Region i Enemy

--- Enemy.cs
+++ Enemy.cs
@@ -8,5 +8,20 @@ namespace Mortens_Komeback_3
 {
     public class Enemy
     {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Method
+
+        #endregion
     }
 }
