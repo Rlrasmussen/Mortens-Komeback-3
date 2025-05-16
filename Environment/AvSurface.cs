bb3d223e97e23b18e1ddb2e042e4b7ea6c8a398f
Philip Eickmayer Hansen <philip-eh@hotmail.com>
2025-05-16 14:20:39 +0200

    Added rotation for collisionbox on AvSurface

--- Environment/AvSurface.cs
+++ Environment/AvSurface.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,19 @@ namespace Mortens_Komeback_3.Environment
         public Texture2D[] Sprites { get; set; }
         public float ElapsedTime { get; set; }
         public int CurrentIndex { get; set; }
+
+        public override Rectangle CollisionBox //Simon
+        {
+            get
+            {
+                if (Sprite != null)
+                    if (Rotation == 0 || Rotation == MathHelper.Pi)
+                        return new Rectangle((int)(Position.X - (Sprite.Width / 2) * scale), (int)(Position.Y - (Sprite.Height / 2) * scale), (int)(Sprite.Width * scale), (int)(Sprite.Height * scale));
+                    else
+                        return new Rectangle((int)(Position.X - (Sprite.Height / 2) * scale), (int)(Position.Y - (Sprite.Width / 2) * scale), (int)(Sprite.Height * scale), (int)(Sprite.Width * scale));
+                else return new Rectangle();
+            }
+        }
 
         #endregion
 
