54b15bd709227ef51212db7ba3dd6a17870a665d
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-05-21 14:58:18 +0200

    Når Playeren collidere med en NPC vises en textbubble

--- NPC.cs
+++ NPC.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System;
+using System.Diagnostics;
+using Mortens_Komeback_3.Command;
+using Mortens_Komeback_3.Collider;
+using Mortens_Komeback_3.Factory;
+using Mortens_Komeback_3.Puzzles;
+using Mortens_Komeback_3.Environment;
+using Mortens_Komeback_3.Menu;
+using Microsoft.Data.Sqlite;
 
 namespace Mortens_Komeback_3
 {
-    public class NPC : GameObject
+    public class NPC : GameObject, ICollidable
     {
         #region Fields
-
+        private Texture2D textBubble = GameWorld.Instance.Sprites[OverlayObjects.InteractBubble][0];
+        private int number = 0;
         #endregion
 
         #region Properties
@@ -23,9 +34,27 @@ namespace Mortens_Komeback_3
             layer = 0.6f;
         }
 
+
         #endregion
 
         #region Method
+        public void OnCollision(ICollidable other)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //If there is a collision between Player and NPC there will spawn an enteract textbubble
+            if ((this as ICollidable).CheckCollision(Player.Instance) && (Player.Instance as IPPCollidable).DoHybridCheck(CollisionBox) && number == 0)
+            {
+                spriteBatch.Draw(textBubble, Position - new Vector2(0, 90), null, drawColor, Rotation, origin, scale, spriteEffect, layer);
+
+
+            }
+
+            base.Draw(spriteBatch);
+        }
 
         #endregion
     }
